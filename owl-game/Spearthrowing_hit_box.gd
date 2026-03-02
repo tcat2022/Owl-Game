c877f8238c1757c395b060eee9cbed92ca4d75b3
Pcblue3 <lizel862@gmail.com>
2026-03-02 16:47:05 -0500

    Projectile Script Done, Melee animation working
    Projectile script is finished, sprite needs an update. Slashing looks ok but we really need an enemy to test hitbox and such. Theres a purple thing on the hitbox thats used to see how big the hitbox is.

--- owl-game/Spearthrowing_hit_box.gd
+++ owl-game/Spearthrowing_hit_box.gd
@@ -14,8 +14,9 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(0, -SPEED).rotated(dir)	
 	move_and_slide()
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		queue_free()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if !(collision.get_collider().name == "owl"):
+			queue_free()
 	
 	
