68a6ab45517e8ef092414fff548a6be24fbb467a
Pcblue3 <lizel862@gmail.com>
2026-04-20 17:25:45 -0400

    Decorate

--- owl-game/Scripts/globals.gd
+++ owl-game/Scripts/globals.gd
@@ -2,7 +2,7 @@ extends Node
 
 var can_pick_up = false
 var can_pick_up_spear = false
-var SpearObtained = true
+var SpearObtained = false
 var WeaponEquipped = false
 var PlayerDamage = 1
 var PlayerKnockback
